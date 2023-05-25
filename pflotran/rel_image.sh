c45000ecf597700b03a4787e2aa94fa4bdf6b28a
Jan Brezina <jan.brezina@tul.cz>
2023-05-25 16:11:26 +0200

    Updated unmodified pflotran image build.

--- pflotran/rel_image.sh
+++ pflotran/rel_image.sh
@@ -1,1 +1,5 @@
-    docker build -t flow123d/pflotran-gnu-rel .
+docker build \
+--build-arg source_repository=https://bitbucket.org/pflotran/pflotran \
+--build-arg commit=083857c \
+-t flow123d/pflotran-gnu-rel ./rel-image
+
