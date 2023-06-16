2df08f5b06ab34cc64245ef353b7c5fa206e93dc
Jan Brezina <jan.brezina@tul.cz>
2023-06-16 18:42:35 +0200

    Verification of the decay calculation

--- pflotran/fterm.sh
+++ pflotran/fterm.sh
@@ -13,7 +13,7 @@ uname="user"
 HOST_ROOT_MOUNT="$ABS_SCRIPT_DIR"
 IMG_ROOT_MOUNT="//home/${uname}/workdir"
 HOST_PWD="`pwd`"
-IMG_PWD="$IMG_ROOT_MOUNT/${HOST_PWD##$HOST_ROOT_MOUNT}"
+IMG_PWD="$IMG_ROOT_MOUNT/${HOST_PWD##$HOST_ROOT_MOUNT/}"
 
 envargs="-euid=$uid -egid=$gid -ewho=$uname -ehome=${IMG_ROOT_MOUNT} -ePETSC_DIR=//usr/local/petsc_v3.18.6/"
 mountargs="-w ${IMG_PWD} -v ${HOST_ROOT_MOUNT}:${IMG_ROOT_MOUNT}"
