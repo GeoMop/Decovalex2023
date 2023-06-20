b08076054854ca38f5d83a3c059c1457aa697b11
Jan Brezina <jan.brezina@tul.cz>
2023-06-20 16:15:12 +0200

    Script fix for windows

--- pflotran/fterm.sh
+++ pflotran/fterm.sh
@@ -10,13 +10,13 @@ ABS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 gid=$(id -g)
 uid=$(id -u)
 uname="user"
-HOST_ROOT_MOUNT="$ABS_SCRIPT_DIR"
-IMG_ROOT_MOUNT="//home/${uname}/workdir"
-HOST_PWD="`pwd`"
+HOST_ROOT_MOUNT="/$ABS_SCRIPT_DIR"
+IMG_ROOT_MOUNT="/home/${uname}/workdir/"
+HOST_PWD="/`pwd`"
 IMG_PWD="$IMG_ROOT_MOUNT/${HOST_PWD##$HOST_ROOT_MOUNT/}"
 
 envargs="-euid=$uid -egid=$gid -ewho=$uname -ehome=${IMG_ROOT_MOUNT} -ePETSC_DIR=//usr/local/petsc_v3.18.6/"
-mountargs="-w ${IMG_PWD} -v ${HOST_ROOT_MOUNT}:${IMG_ROOT_MOUNT}"
+mountargs="-w /${IMG_PWD} -v ${HOST_ROOT_MOUNT}:${IMG_ROOT_MOUNT}"
 interactive="--rm -it"
 #image=flow123d/flow-dev-gnu-rel:4.0.3
 image=${PFLOTRAN_IMAGE:-flow123d/pflotran-gnu-rel}
